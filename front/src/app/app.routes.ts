231d6815aada578ff622302d97c9f707fd8ce5bf
Alícia B Mendes <aliealie.am@gmail.com>
2024-09-04 17:46:08 -0300

    feat: started the frontend

---
+++ front/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+    title: 'ToDo - List',
+  },
+];
